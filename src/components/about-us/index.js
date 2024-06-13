25ee6b4b3bc4dbceb0aee0c6e17a9e4c069f7853
mudassir7732 <mudassir7732@gmail.com>
2024-06-13 11:36:24 +0500

    Updated code

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -3,39 +3,55 @@
 const AboutUs = () => {
     return (
         // <div className="flex flex-col md:flexrow">
-        <div className="flex flex-col-reverse xl:flex-row justify-center items-center px-[30px] sm:px-[100px] md:px-[130px] pb-[101px] gap-x-[135px]">
-            {/* <div className="w-fit">
-                <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
-                <button className="text-white border-[1px] text-[14px] font-[300] font-[Montserrat] border-white absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-black">
-                    About Us
-                </button>
-            </div> */}
+        <div className="flex flex-col lg:flex-row justify-between sm:py-[70px] md:py-[95px] px-[40px] sm:px-[70px] md:px-[100px] w-full border-2">
 
-            <div className=" flex flex-col pt-[148px] w-fit xl:w-[45%] border-2">
-                <img src="/assets/images/Rectangle 65.png" alt="image" className="w-[300px] sm:w-[400px] md:w-[558px] md:h-[400px] xl:w-[519px] xl:h-[372px]" />
-                <img src="/assets/images/Rectangle 66.png" alt="image" className="w-[160px] sm:w-[200px] h-fit md:w-[305px] md:h-[332px] absolute ml-[165px] sm:ml-[224px] md:ml-[285px] lg:ml-[294px] mt-[156px] sm:mt-[200px] md:mt-[238px] xl:mt-[211px] mr-44" />
 
-                <div className="bg-[#006fff] rounded-[17px] w-[160px] sm:w-[220px] md:w-[283px] md:h-[150px] mt-[10px] py-[15px] md:py-[23px] px-[15px] sm:px-[18px] md:px-[30px]">
-                    <img src="/assets/icons/completed.svg" alt="image" className="w-[25px] h-[25px] sm:w-[30px] sm:h-[30px] md:w-[46px] md:h-[46px]" />
-                    <p className="text-[15px] sm:text-[18px] md:text-[24px] font-[700] font-[Montserrat] text-white mt-[6px]">
-                        100+ Projects
-                    </p>
-                    <p className="text-[12px] sm:text-[14px] md:text-[18px] font-[400] font-[Montserrat] text-white">
-                        We Have Completed
-                    </p>
+
+            <div className="flex flex-col items-start  w-full xl:w-[47%] pt-[85px]">
+                <div className="flex flex-col items-center absolute">
+                    <img src="/assets/images/Rectangle 65.png" alt="image" className="z-0 w-[300px] sm:w-[400px] md:w-[450px] xl:w-[480px]" />
+                    <img src="/assets/logo/Logo.svg" alt="image" className=" mt-[19px] absolute z-10 w-[40px] sm:w-[50px] md:w-[60px] xl:w-[60px]" />
+                </div>
+
+                <div className="flex flex-row items-end w-full z-50 mt-[186px]">
+                    <div className="bg-[#006fff] rounded-[17px] md:h-[150px] max-w-[250px] mb-2 mt-[10px] py-[15px] md:py-[23px] px-[15px] sm:px-[18px] md:px-[20px]">
+                        <img src="/assets/icons/completed.svg" alt="image" className="w-[25px] h-[25px] sm:w-[30px] sm:h-[30px] md:w-[46px] md:h-[46px]" />
+                        <p className="text-[15px] sm:text-[18px] md:text-[24px] w-full font-[700] font-[Montserrat] text-white mt-[6px]">
+                            100+ Projects
+                        </p>
+                        <p className="text-[12px] sm:text-[14px] md:text-[18px] font-[400] font-[Montserrat] text-white">
+                            We Have Completed
+                        </p>
+                    </div>
+                    <img src="/assets/images/Rectangle 66.png" alt="image" className="w-[300px]"/>
                 </div>
             </div>
 
-            <div className="flex flex-col pt-[78px] w-full lg:w-[45%] pl-8">
 
-                <p className="text-[38px] font-[700] text-black font-[Montserrat] leading-10 pt-[60px]">
+
+
+
+
+
+
+
+
+            <div className="flex flex-col w-full lg:w-[47%] ">
+                <div className="w-fit hidden lg:block">
+                    <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
+                    <button className="text-black border-[1px] text-[14px] font-[300] font-[Montserrat] border-black absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-white">
+                        About Us
+                    </button>
+                </div>
+
+                <p className="text-[38px] font-[700] text-black font-[Montserrat] pt-[60px]">
                     Connecting your business with the world
                 </p>
-                <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[22px]">
+                <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[15px]">
                     Lorem ipsum dummy is simply dummy text of the printing and estting  industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500s when
                 </p>
 
-                <div className="flex flex-col w-[500px]">
+                <div className="flex flex-col lg:w-[500px]">
                     <div className="flex flex-row items-center mt-[13px] gap-x-[10px]">
                         <img src="/assets/icons/trophy.svg" alt="icon" className="h-[50px] w-[59px]" />
                         <p className="text-[18px] font-[400] font-[Montserrat]">
@@ -62,8 +78,8 @@ const AboutUs = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center gap-x-[23px] h-[53px] mt-[40px]">
-                    <button className="bg-[#006fff] outline-none text-white rounded-[10px] w-[268px] h-[53px] text-[18px] font-[500]">
+                <div className="flex flex-row items-center gap-x-[15px] sm:gap-x-[19px] lg:gap-x-[23px] h-[53px] mt-[20px] sm:mt-[30px] lg:mt-[40px]">
+                    <button className="bg-[#006fff] outline-none text-white rounded-[10px] w-[190px] sm:w-[220px] lg:w-[268px] h-[40px] sm:h-[45px] lg:h-[53px] text-[17px] lg:text-[18px] font-[400] lg:font-[500]">
                         Learn More About Us
                     </button>
                     <p className="cursor-pointer underline text-[18px] font-[400] font-[Montserrat]">
