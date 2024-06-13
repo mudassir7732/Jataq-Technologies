652c1abd1c49e80968f6dbea50e3d36cd5f7d15f
mudassir7732 <mudassir7732@gmail.com>
2024-06-13 15:46:30 +0500

    Updated code

--- src/sections/about-us/index.js
+++ src/sections/about-us/index.js
@@ -3,7 +3,7 @@
 const AboutUs = () => {
     return (
         <div className="flex flex-col lg:flex-row lg:justify-between xl:justify-evenly sm:py-[70px] md:py-[95px] px-[40px] sm:px-[70px] md:px-[100px] w-full">
-            <div className="w-fit block lg:hidden">
+            <div className="w-fit block lg:hidden mt-12">
                 <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
                 <p className="flex items-center justify-center text-black border-[1px] text-[14px] font-[300] font-[Montserrat] border-black absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-white">
                     About Us
@@ -28,7 +28,7 @@ const AboutUs = () => {
                     <img src="/assets/images/Rectangle 66.png" alt="image" className="w-[150px] sm:w-[230px] md:w-[220px] xl:w-[300px]" />
                 </div>
             </div>
-            <div className="flex flex-col w-full lg:w-[47%]">
+            <div className="flex flex-col w-full lg:w-[47%] pt-8">
                 <div className="w-fit hidden lg:block">
                     <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
                     <button className="text-black border-[1px] text-[14px] font-[300] font-[Montserrat] border-black absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-white">
