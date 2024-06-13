b3a64544dad8f8cc53ee62d48ea83e5f7d199479
mudassir7732 <mudassir7732@gmail.com>
2024-06-13 14:59:05 +0500

    Updated code

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,18 +1,65 @@
 
 const Footer = () => {
     return (
-        <div className="bg-[#0a0b0f]">
-            <div className='flex flex-row items-center w-fit gap-x-[8px]'>
-                <img src="/assets/logo/Logo.svg" alt='logo' className='w-[28px] sm:w-[30px] md:w-[32px] lg:w-[34px] xl:w-[36px] h-[30px] sm:h-[32px] md:h-[34px] lg:h-[36px] xl:h-[38px]' />
-                <div className='flex flex-col'>
-                    <p className='font-[Montserrat] font-[700] text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] text-[#252525]'>
-                        JATAQ
-                    </p>
-                    <p className='font-[Montserrat] text-[8px] sm:text-[8.5px] md:text-[9px] lg:text-[9.5px] xl:text-[10px] -mt-1 font-[300] text-[#252525]'>
-                        TECHNOLOGIES
-                    </p>
+        <div className="bg-[#0a0b0f] pl-[100px] pt-[86px] pr-[132px] pb-[48px]">
+
+            <div className="flex flex-col lg:flex-row items-center justify-between">
+
+                <div className='flex flex-row items-center w-fit gap-x-[8px]'>
+                    <img src="/assets/logo/Logo.svg" alt='logo' className='w-[28px] sm:w-[30px] md:w-[32px] lg:w-[34px] xl:w-[36px] h-[30px] sm:h-[32px] md:h-[34px] lg:h-[36px] xl:h-[38px]' />
+                    <div className='flex flex-col'>
+                        <p className='font-[Montserrat] font-[700] text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] text-[#fff]'>
+                            JATAQ
+                        </p>
+                        <p className='font-[Montserrat] text-[8px] sm:text-[8.5px] md:text-[9px] lg:text-[9.5px] xl:text-[10px] -mt-1 font-[300] text-[#fff]'>
+                            TECHNOLOGIES
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex flex-row items-center w-fit gap-x-[35px]">
+                    <div className="flex flex-row items-center gap-x-1">
+                        <img src="/assets/icons/facebook-fill.svg" alt='facebook_icon' className="h-[24px] w-[24px]" />
+                        <p className="text-white text-[18px] font-[400] font-[Montserrat]">
+                            Facebook
+                        </p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-x-1">
+                        <img src="/assets/icons/linkedin-fill.svg" alt='facebook_icon' className="-mt-1 h-[24px] w-[24px]" />
+                        <p className="text-white text-[18px] font-[400] font-[Montserrat]">
+                            LinkedIn
+                        </p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-x-1">
+                        <img src="/assets/icons/instagram.svg" alt='facebook_icon' className="h-[24px] w-[24px]" />
+                        <p className="text-white text-[18px] font-[400] font-[Montserrat]">
+                            Instagram
+                        </p>
+                    </div>
                 </div>
             </div>
+
+            <div className="flex flex-row mt-[50px] border-b-[1px] border-t-[1px] border-white">
+                <div className="flex flex-col pt-[98px]">
+                    <p className="text-[38px] font-[700] text-white font-[Montserrat]">
+                        Have an idea
+                    </p>
+                    <p className="text-[38px] font-[700] text-white font-[Montserrat]">
+                        Tell us about it
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <img  />
+                        <p className="text-[18px] font-[400] text-white">
+                            Contact@jataq.co
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
