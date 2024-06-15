57fd61982c8ff65f6c9491a2d1750c64c93cd838
mudassir7732 <mudassir7732@gmail.com>
2024-06-15 17:26:56 +0500

    Updated code

--- src/sections/services/index.js
+++ src/sections/services/index.js
@@ -6,7 +6,7 @@ const Services = () => {
     return (
         <div className="flex flex-col lg:flex-row items-start justify-evenly lg:justify-between xl:justify-evenly py-[30px] sm:py-[70px] md:py-[95px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] h-[980px] lg:h-[663px] w-full" style={{ backgroundImage: 'url(/assets/images/bg.svg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
 
-            <div className="flex flex-col w-full lg:w-[47%]">
+            <div className="flex flex-col w-full lg:w-[45%] max-w-[507px]">
                 <div className="w-fit">
                     <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
                     <p className="flex items-center justify-center text-black border-[1px] text-[14px] font-[300] font-[Montserrat] border-black absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-white">
@@ -31,7 +31,8 @@ const Services = () => {
                 </button>
             </div>
 
-            <div className="flex flex-col w-full lg:w-[47%] cursor-pointer">
+            <div className="flex flex-col w-fit items-center gap-x-12 lg:w-[47%] cursor-pointer">
+                <div className="w-fit">
                 <div className="flex flex-row items-center mt-[34px]" onClick={() => setSelected('web-design')}>
                     <div className={`flex items-center justify-center min-h-[88px] min-w-[88px] ${selected === 'web-design' ? 'bg-[#006fff]' : 'bg-[#272727]'} rounded-[10px]`}>
                         <img src="/assets/icons/web-design.svg" alt='' className="h-[55px] w-[55px]" />
@@ -77,6 +78,7 @@ const Services = () => {
                             Graphic Design
                         </p>
                         <img src='/assets/icons/arrow.svg' alt='arrow_icon' className="h-[12px] sm:h-[13px] md:h-[14px] lg:h-[15px] xl:h-[16.25px]" />
+                    </div>
                     </div>
                 </div>
 
