ff10bc1abb356c59a18e4dbb43f65950e98d78fd
mudassir7732 <mudassir7732@gmail.com>
2024-06-16 14:00:28 +0500

    Updated code

--- src/sections/services/index.js
+++ src/sections/services/index.js
@@ -4,7 +4,7 @@ const Services = () => {
     const [selected, setSelected] = useState('');
 
     return (
-        <div className="background-container flex flex-col lg:flex-row items-start justify-evenly lg:justify-between xl:justify-evenly py-[30px] sm:py-[70px] md:py-[95px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] bg-[#0a0b0f] xl:px-[115px] h-[980px] lg:h-[663px] w-full"
+        <div className="background-container flex flex-col lg:flex-row items-start justify-evenly lg:justify-between xl:justify-evenly py-[30px] sm:py-[70px] md:py-[95px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] bg-[#0a0b0f] xl:px-[115px] w-full"
             style={{ backgroundImage: 'url(/assets/logo/bg-logo3.png)', backgroundSize: '70% auto', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
             <div className="flex flex-col w-full lg:w-[45%] max-w-[440px]">
                 <div className="w-fit">
