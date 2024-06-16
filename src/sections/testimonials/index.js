2ae49a65d2106fd0e67e9f96597b2bdb47e5fcfb
mudassir7732 <mudassir7732@gmail.com>
2024-06-16 12:35:30 +0500

    Updated code

--- src/sections/testimonials/index.js
+++ src/sections/testimonials/index.js
@@ -32,8 +32,8 @@ const Testimonials = () => {
     return (
         <div className="relative pb-20 lg:h-fit background-dynamic">
             <img src='/assets/icons/Ellipse8.svg' alt="border_line" className="-mt-20 w-full h-full absolute" />
-            <div className="flex flex-col lg:flex-row items-start mt-48 gap-x-[35px] lg:justify-evenly border-white h-ful px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px]">
-                <div className="relative flex flex-col min-w-[45%]">
+            <div className="flex flex-col lg:flex-row items-start mt-48 gap-x-[35px] lg:justify-between xl:justify-evenly border-white h-ful px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px]">
+                <div className="relative flex flex-col w-fit">
                     <img src='/assets/icons/Rectangle58.svg' alt='button_background' className="absolute z-0" />
                     <p className="flex items-center justify-center text-black border-[1px] text-[14px] font-[300] font-[Montserrat] border-black absolute z-50 w-[119px] h-[31px] mt-2 rounded-full bg-white">
                         Testimonials
