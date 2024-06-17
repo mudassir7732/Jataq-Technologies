9357a015207b9d32b4b67f6b03e28ea5a5cd1dc4
mudassir7732 <mudassir7732@gmail.com>
2024-06-17 16:05:16 +0500

    Updated code

---
+++ src/constants/workOptions.js
@@ -0,0 +1,8 @@
+export const WORK_OPTIONS=[
+    {title:'Schedule an Initial Consultation', 
+    paragraph:"Contact us to discuss your project. We'll cover team structure, objectives, budget, schedule, and the skills you need."},
+    {title:'Schedule an Initial Consultation', 
+    paragraph:"Contact us to discuss your project. We'll cover team structure, objectives, budget, schedule, and the skills you need."},
+    {title:'Schedule an Initial Consultation', 
+    paragraph:"Contact us to discuss your project. We'll cover team structure, objectives, budget, schedule, and the skills you need."},
+]
