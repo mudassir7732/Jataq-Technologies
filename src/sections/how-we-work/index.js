4e9f084aaae0fc02b043d75e5f74f8484b1597de
mudassir7732 <mudassir7732@gmail.com>
2024-06-17 16:07:51 +0500

    Updated code

--- src/sections/how-we-work/index.js
+++ src/sections/how-we-work/index.js
@@ -1,3 +1,5 @@
+import { WORK_OPTIONS } from "../../constants/workOptions";
+
 const WorkMethod = () => {
     return (
         <div className="flex flex-col items-center px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] pt-20">
@@ -26,9 +28,9 @@ const WorkMethod = () => {
                     </p>
                 </div>
                 <div className="hidden md:grid grid-cols-1 md:grid-cols-3 w-fit md:w-full h-[43px]">
-                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]"/>
-                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]"/>
-                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]"/>
+                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]" />
+                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]" />
+                    <div className="border-l-[1px] border-[#d7d7d7] ml-[20px]" />
                 </div>
                 <div className="hidden md:grid grid-cols-1 md:grid-cols-3 w-fit md:w-full">
                     <img src="/assets/icons/arrow-head.svg" alt="arrow_head" className="h-[16px] w-[16px] ml-[13px] -mt-[1px]" />
@@ -37,14 +39,16 @@ const WorkMethod = () => {
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 mt-[15px] lg:mt-[30px] gap-4 w-full mb-[52px] lg:mb-[152px]">
-                    <div className="flex flex-col">
-                        <p className="text-[21px] lg:text-[22px] xl:text-[24px] font-[700] font-[Montserrat] text-[#252525]">
-                            Schedule an Initial Consultation
-                        </p>
-                        <p className="text-[16px] md:text-[17px] xl:text-[18px] text-black mt-[15px] font-[400] font-[Montserrat]">
-                            Contact us to discuss your project. We'll cover team structure, objectives, budget, schedule, and the skills you need.
-                        </p>
-                    </div>
+                    {WORK_OPTIONS?.map((item, index) => (
+                        <div className="flex flex-col">
+                            <p className="text-[21px] lg:text-[22px] xl:text-[24px] font-[700] font-[Montserrat] text-[#252525]">
+                                {item?.title}
+                            </p>
+                            <p className="text-[16px] md:text-[17px] xl:text-[18px] text-black mt-[15px] font-[400] font-[Montserrat]">
+                                {item?.paragraph}
+                            </p>
+                        </div>
+                    ))}
                     <div className="flex flex-col">
                         <p className="text-[21px] lg:text-[22px] xl:text-[24px] font-[700] font-[Montserrat] text-[#252525]">
                             Discuss Solution and Team Structure
