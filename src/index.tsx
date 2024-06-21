8d0d7940c5939fcf376f71e0832262081ad80f89
mudassir7732 <mudassir7732@gmail.com>
2024-06-21 23:46:29 +0500

    Updated code

--- src/index.tsx
+++ src/index.tsx
@@ -20,10 +20,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
-if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
       <App />
   );
-}
 
