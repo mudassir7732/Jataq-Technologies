09fd8e69fdf61648dfe4c11ab46e3fd0c9d4b268
mudassir7732 <mudassir7732@gmail.com>
2024-06-21 23:47:05 +0500

    Updated code

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,6 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
   const root = ReactDOM.createRoot(rootElement);
