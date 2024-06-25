93dc610ce865c627b789d27b0b4374d9dc274df9
mudassir7732 <mudassir7732@gmail.com>
2024-06-25 12:08:58 +0500

    Started About Us page design

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,8 +3,11 @@ import { FC } from "react";
 const About: FC = () => {
     return (
         <div className="flex flex-col">
-            <p className="text-[55px] font-[700] w-full text-center font-[Montserrat] text-[#000000] mt-[76px]">
+            <p className="text-[55px] font-[700] mx-auto font-[Montserrat] text-[#000000] mt-[76px]">
                 About Us
+            </p>
+            <p className="text-[18px] font-[400] max-w-[567px] mx-auto text-center font-[Montserrat] mb-[63px]">
+                Discover our story, our mission, and our passion for creativity and innovation.
             </p>
         </div>
     )
