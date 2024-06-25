b17be2184fa70b6c05c1a578b2d85686e65182b4
mudassir7732 <mudassir7732@gmail.com>
2024-06-25 11:11:05 +0500

    Updated code

--- src/constants/serviceOptions.ts
+++ src/constants/serviceOptions.ts
@@ -1,7 +1,7 @@
 import { ServiceProps } from "../types/serviceOptions";
 
 export const SERVICE_OPTIONS: ServiceProps[] = [
-    { icon: '/assets/icons/web-design.svg', label: 'Web Development/<br /> Web Design', route: '/web-design' },
+    { icon: '/assets/icons/web-design.svg', label: 'Web Development/ Web Design', route: '/web-design' },
     { icon: '/assets/icons/first.svg', label: 'Digital Marketing', route: '/digital-marketing' },
     { icon: '/assets/icons/mobile.svg', label: 'App Development', route: '/app-development' },
     { icon: '/assets/icons/first2.svg', label: 'Graphic Design', route: '/graphic-design' },
--- src/sections/services/styles.ts
+++ src/sections/services/styles.ts
@@ -1,15 +1,15 @@
 const styles = {
     container: "background-container flex flex-col lg:flex-row bg-no-repeat bg-cover bg-center items-start justify-evenly lg:justify-between xl:justify-evenly py-[30px] sm:py-[70px] md:py-[95px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] bg-[#0a0b0f] xl:px-[115px] w-full",
-    first_section: "flex flex-col w-full lg:w-[45%] max-w-[440px]",
+    first_section: "flex flex-col items-start w-fit xl:max-w-[47%]",
     heading: "text-white text-[26px] sm:text-[28px] md:text-[30px] lg:text-[35px] xl:text-[38px] font-[700] font-[Montserrat] mt-[60px]",
-    text: "w-full justify-center flex-wrap text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white mt-[26px] font-[400] font-[Montserrat]",
+    text: "w-full justify-center flex-wrap text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white mt-[26px] font-[400] font-[Montserrat] max-w-[85%]",
     yellow_text: "pl-[6px] pr-[4px] text-[#ffe000] font-[400] font-[Montserrat]  text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px]",
     button: "flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[32px] sm:h-[40px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-2 xl:w-[257px] bg-[#1b7eff] rounded-[10px] mt-[28px] md:mt-[48px] text-[13px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white font-[500] font-[Montserrat]",
-    second_section: "flex flex-col items-center max-w-full gap-x-12 lg:w-[47%] cursor-pointer mt-6 sm:mt-10 md:mt-16 lg:mt-0",
-    service_wrapper: "flex flex-row items-center mt-[20px] md:mt-[34px]",
-    icon_wrapper: "flex items-center justify-center min-h-[60px] sm:min-h-[70px] md:min-h-[88px] min-w-[60px] sm:min-w-[70px] md:min-w-[88px] rounded-[10px]",
-    icon: "h-[40px] md:h-[55px] w-[40px] md:w-[55px]",
-    text_wrapper: "flex flex-row items-center mx-[20px] md:mx-[41px] justify-between h-[88px] min-w-[70%] sm:min-w-[85%] md:max-w-[200px] lg:w-[328px] border-b-[1px] border-[#4e4e4e] pb-[15px]",
+    second_section: "flex flex-col w-full lg:max-w-[47%]",
+    service_wrapper: "flex flex-row items-center mt-[20px] md:mt-[34px] cursor-pointer",
+    icon_wrapper: "flex items-center justify-center min-h-[60px] sm:min-h-[70px] md:min-h-[88px] lg:min-h-[60px] xl:min-h-[88px] min-w-[60px] sm:min-w-[70px] md:min-w-[88px] lg:min-w-[60px] xl:min-w-[88px] rounded-[10px]",
+    icon: "h-[40px] md:h-[55px] lg:w-[40px] xl:w-[55px] w-[40px] md:w-[55px] lg:w-[40px] xl:w-[55px]",
+    text_wrapper: "flex flex-row items-center mx-[20px] md:mx-[41px] justify-between border-2 h-[88px] min-w-[70%] sm:min-w-[85%] md:max-w-[200px] lg:w-fit border-b-[1px] border-[#4e4e4e] pb-[15px]",
     service_text: "text-white text-[16px] md:text-[17px] xl:text-[18px] font-[400] font-[Montserrat]",
     arrow_icon: "h-[12px] sm:h-[13px] md:h-[14px] lg:h-[15px] xl:h-[16.25px]"
 }
