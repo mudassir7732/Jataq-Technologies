03b2d34c8c61d6118caf3aedb769e1ba1b2752e9
mudassir7732 <mudassir7732@gmail.com>
2024-06-25 19:08:15 +0500

    Created New Page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,35 @@
 import { FC } from "react";
+import Testimonials from "../../sections/testimonials";
+import ServicesBar from "../../sections/services-bar";
+import WorkMethod from "../../sections/how-we-work";
 
 const About: FC = () => {
     return (
         <div className="flex flex-col">
-            <p className="text-[55px] font-[700] mx-auto font-[Montserrat] text-[#000000] mt-[76px]">
-                About Us
-            </p>
-            <p className="text-[18px] font-[400] max-w-[567px] mx-auto text-center font-[Montserrat] mb-[63px]">
-                Discover our story, our mission, and our passion for creativity and innovation.
-            </p>
+            <div className='px-[100px]'>
+                <p className="text-[55px] font-[700] mx-auto text-center font-[Montserrat] text-[#000000] mt-[76px]">
+                    About Us
+                </p>
+                <p className="text-[18px] font-[400] max-w-[567px] mx-auto text-center font-[Montserrat] mb-[63px]">
+                    Discover our story, our mission, and our passion for creativity and innovation.
+                </p>
+
+                <div className="bg-[#006fff] min-h-[398px] rounded-[20px]">
+                    <div className="w-1/2 flex px-[50px] py-[85px]">
+                        <p className="text-[36px] font-[700]">
+                            We help our client succeed by creating identities, digital experiences and connecting their business with the world
+                        </p>
+                    </div>
+                    <div className="w-1/2">
+
+                    </div>
+                </div>
+
+            </div>
+
+            <WorkMethod />
+            <Testimonials />
+            <ServicesBar />
         </div>
     )
 }
