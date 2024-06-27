d330b4835a3923047b3778e11ecac26881cb3c29
mudassir7732 <mudassir7732@gmail.com>
2024-06-27 05:48:11 +0500

    Resolve side padding of Services section

--- src/sections/services/styles.ts
+++ src/sections/services/styles.ts
@@ -1,11 +1,11 @@
 const styles = {
-    container: "background-container flex flex-col lg:flex-row bg-no-repeat bg-cover bg-center items-start justify-between xl:justify-evenly py-[30px] sm:py-[70px] md:py-[95px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] bg-[#0a0b0f] xl:px-[115px] w-full",
+    container: "flex flex-col lg:flex-row bg-no-repeat bg-cover bg-center items-start justify-between py-[30px] sm:py-[70px] md:py-[95px] bg-[#0a0b0f] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] w-full",
     first_section: "flex flex-col items-start w-fit xl:max-w-[47%]",
     heading: "text-white text-[26px] sm:text-[28px] md:text-[30px] lg:text-[35px] xl:text-[38px] font-[700] font-[Montserrat] mt-[60px]",
     text: "w-full justify-center flex-wrap text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white mt-[26px] font-[400] font-[Montserrat] max-w-[85%]",
     yellow_text: "pl-[6px] pr-[4px] text-[#ffe000] font-[400] font-[Montserrat]  text-[14px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px]",
     button: "flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[32px] sm:h-[40px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-2 xl:w-[257px] bg-[#1b7eff] rounded-[10px] mt-[28px] md:mt-[48px] text-[13px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white font-[500] font-[Montserrat]",
-    second_section: "flex flex-col w-full lg:max-w-[47%] mt-4 lg:mt-0",
+    second_section: "flex flex-col mt-4 lg:mt-0 w-[450px]",
     service_wrapper: "flex flex-row items-center mt-[20px] md:mt-[34px] lg:mt-[25px] xl:mt-[35px] cursor-pointer",
     icon_wrapper: "flex items-center justify-center min-h-[60px] sm:min-h-[70px] md:min-h-[88px] lg:min-h-[70px] xl:min-h-[88px] min-w-[60px] sm:min-w-[70px] md:min-w-[88px] lg:min-w-[70px] xl:min-w-[88px] rounded-[10px]",
     icon: "h-[40px] md:h-[55px] lg:w-[40px] xl:w-[55px] w-[40px] md:w-[55px] lg:w-[45px] xl:w-[55px]",
