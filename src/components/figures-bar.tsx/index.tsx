7bfec3c063a5190d6839defcb0d8a461aefaaf4b
mudassir7732 <mudassir7732@gmail.com>
2024-06-27 18:25:37 +0500

    Web Development Page completed

--- src/components/figures-bar.tsx/index.tsx
+++ src/components/figures-bar.tsx/index.tsx
@@ -3,7 +3,7 @@ const FiguresBar = () => {
     return (
         <div style={{ backgroundImage: 'url(/assets/images/figures_bg.png)' }} className="px-[40px] sm:px-[70px] md:px-[80px] lg:px-[95px] xl:px-[113px] bg-no-repeat bg-cover bg-center items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-fit gap-x-[106px] gap-y-[40px] py-[46px] min-h-[156px] rounded-[10px]">
             <div className="flex flex-col min-w-[185px]">
-                <p className="text-[38px] font-[700] font-[Montserrat] text-black">
+                <p className="text-[26px] sm:text-[31px] md:text-[34px] lg:text-[36px] xl:text-[38px] font-[700] font-[Montserrat] text-black">
                     10+
                 </p>
                 <p className="text-[15px] font-[400] text-black font-[Montserrat]">
@@ -12,7 +12,7 @@ const FiguresBar = () => {
             </div>
 
             <div className="flex flex-col min-w-[185px]">
-                <p className="text-[38px] font-[700] font-[Montserrat] text-black">
+                <p className="text-[26px] sm:text-[31px] md:text-[34px] lg:text-[36px] xl:text-[38px] font-[700] font-[Montserrat] text-black">
                     108+
                 </p>
                 <p className="text-[15px] font-[400] text-black font-[Montserrat]">
@@ -20,7 +20,7 @@ const FiguresBar = () => {
                 </p>
             </div>
             <div className="flex flex-col min-w-[185px]">
-                <p className="text-[38px] font-[700] font-[Montserrat] text-black">
+                <p className="text-[26px] sm:text-[31px] md:text-[34px] lg:text-[36px] xl:text-[38px] font-[700] font-[Montserrat] text-black">
                     306
                 </p>
                 <p className="text-[15px] font-[400] text-black font-[Montserrat]">
--- src/pages/web-dev/index.tsx
+++ src/pages/web-dev/index.tsx
@@ -12,28 +12,28 @@ const WebDev: FC = () => {
                 <p className="text-[35px] sm:text-[40px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-[700] mx-auto w-full text-center font-[Montserrat] text-[#000000] mt-[76px]">
                     Web app development
                 </p>
-                <p className="text-[18px] font-[400] max-w-[775px] mx-auto text-center font-[Montserrat] mb-[63px] mt-4 sm:mt-2 md:mt-0">
+                <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] max-w-[775px] mx-auto text-center font-[Montserrat] mb-[63px] mt-4 sm:mt-2 md:mt-0">
                     Leverage our experienced our experienced team for efficient and high-quality web app development. We deliver scalable solutions within your budget, from concept to launch, to help your business grow.
                 </p>
             </div>
             <div style={{ backgroundImage: 'url(/assets/images/Rectangle69.png)' }} className="bg-center flex flex-col  bg-no-repeat bg-cover mb-[50px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] gap-y-[116px] pt-[36px] sm:pt-[42px] md:pt-[48px] lg:pt-[55px] xl:pt-[66px]">
                 <div className="flex flex-col lg:flex-row items-start justify-between">
                     <div className="w-full lg:w-1/2 pr-[20px] lg:pr-0">
-                        <p className="text-[38px] font-[700]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700]">
                             Our cost-effective web development delivers quality within budget.
                         </p>
-                        <p className="mt-[19px] text-[18px] font-[400] text-black max-w-[88%]">
+                        <p className="mt-[19px] text-[16px] sm:text-[17px] lg:text-[18px] font-[400] text-black max-w-[88%]">
                             At Jataq Technologies, our expert teams bring unbeatable expertise and deep industry knowledge to every progressive web app project.
                         </p>
-                        <button className="flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[32px] sm:h-[40px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-2 lg:px-3 xl:w-[257px] bg-[#1b7eff] rounded-[20px] mt-[34px] text-[13px] sm:text-[15px] md:text-[16px] lg:text-[17px] xl:text-[18px] text-white font-[500] font-[Montserrat]">
+                        <button className="flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[40px] sm:h-[45px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-4 lg:px-3 xl:w-[257px] bg-[#1b7eff] rounded-[10px] sm:rounded-[12px] md:rounded-[15px] lg:rounded-[20px] mt-[34px] text-[13px] md:text-[16px] xl:text-[16px] sm:text-[17px] lg:text-[18px] text-white font-[500] font-[Montserrat]">
                             Schedule a Meeting
                             <img src='/assets/icons/arrow.svg' alt='arrow_icon' className="h-[12px] sm:h-[13px] md:h-[14px] lg:h-[15px] xl:h-[16.25px]" />
                         </button>
                     </div>
                     <div className="w-full lg:w-1/2 flex flex-col gap-y-[18px] mt-12 lg:mt-0">
                         {WEB_FEATURES?.map((feature: WebFeatureProps, index: number) => (
-                            <div key={index} className={`flex flex-row items-center gap-x-[39px] px-[27px] py-[31px] ${index === 0 ? 'bg-[#006fff]' : 'bg-white'} rounded-[20px]`}>
-                                <img src={feature?.icon} className={`${index === 0 ? 'w-[39px]' : 'w-[55px]'} ${index === 0 ? 'h-[39px]' : 'h-[55px]'}`} />
+                            <div key={index} className={`flex flex-row items-center gap-x-[20px] md:gap-x-[39px] px-[15px] sm:px-[18px] md:px-[22px] lg:px-[27px] py-[12px] sm:py-[17px] md:py-[24px] lg:py-[31px] ${index === 0 ? 'bg-[#006fff]' : 'bg-white'} rounded-[20px]`}>
+                                <img src={feature?.icon} className={`${index === 0 ? 'w-[30px] md:w-[39px]' : 'w-[42px] md:w-[55px]'} ${index === 0 ? 'h-[30px] md:h-[39px]' : 'h-[42px] md:h-[55px]'}`} />
                                 <p className={`text-[22px] font-[500] font-[Montserrat] ${index === 0 ? 'text-white' : 'text-black'}`}>
                                     {feature?.label}
                                 </p>
@@ -45,109 +45,109 @@ const WebDev: FC = () => {
             </div>
 
             <div className="w-full mt-[107px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px]">
-                <p className="text-[38px] font-[700] font-[Montserrat] max-w-[835px] mx-auto text-center">
+                <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] font-[Montserrat] max-w-[835px] mx-auto text-center">
                     Managing The Full Software Development Lifecycle
                 </p>
-                <p className="text-[18px] font-[400] font-[Montserrat] text-center mt-[17px]">
+                <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-center mt-[17px]">
                     Jataq Technologies provides end-to-end web app development.
                 </p>
-                {/* <div className="grid grid-cols-3 mt-[60px]">
-                    <div className="border-[#d2d2d2] pr-[24px] pb-[29px] border-b-[1px] border-r-[1px]">
-                        <p className="text-[38px] font-[700] text-[#006fff]">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-[38px] sm:mt-[45px] md:mt-[60px]">
+                    <div className="border-[#d2d2d2] pr-[24px] pb-[15px] sm:pb-[29px] sm:border-b-[1px] sm:border-r-[1px]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#006fff]">
                             01
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                    <div className="border-[#d2d2d2] px-[24px] pb-[29px] border-b-[1px] border-r-[1px]">
-                        <p className="text-[38px] font-[700] text-[#d2d2d2]">
+                    <div className="border-[#d2d2d2] sm:pl-[24px] lg:px-[24px] pb-[15px] sm:pb-[29px] sm:border-b-[1px] lg:border-r-[1px]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
                             02
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                    <div className="border-[#d2d2d2] px-[24px] pb-[29px] border-b-[1px]">
-                        <p className="text-[38px] font-[700] text-[#d2d2d2]">
+                    <div className="border-[#d2d2d2] sm:pr-[24px] lg:pl-[24px] pb-[15px] sm:pb-[29px] sm:border-b-[1px] sm:border-r-[1px] lg:border-r-0">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
                             03
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                    <div className="border-[#d2d2d2] px-[24px] py-[32px] border-r-[1px]">
-                        <p className="text-[38px] font-[700] text-[#d2d2d2]">
+                    <div className="border-[#d2d2d2] sm:pl-[24px] lg:pr-[24px] lg:pl-0 lg:border-r-[1px] sm:border-b-[1px] lg:border-b-0 pb-[15px] sm:pb-[29px]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
                             04
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                    <div className="border-[#d2d2d2] px-[24px] py-[32px] border-r-[1px]">
-                        <p className="text-[38px] font-[700] text-[#d2d2d2]">
+                    <div className="border-[#d2d2d2] pr-[24px] lg:px-[24px] sm:border-r-[1px] pb-[15px] sm:pb-[29px]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
                             05
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                    <div className="border-[#d2d2d2] px-[24px] py-[32px]">
-                        <p className="text-[38px] font-[700] text-[#d2d2d2]">
+                    <div className="border-[#d2d2d2] sm:pl-[24px] pt-0 sm pb-[15px] sm:pb-[29px]">
+                        <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
                             06
                         </p>
-                        <p className="text-[24px] font-[700] text-[#252525] mt-[7px]">
+                        <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             Design
                         </p>
-                        <p className="text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
+                        <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] font-[Montserrat] text-black mt-[18px]">
                             Our dedicated design team starts with creating a beautiful UI/UX design, laying a solid foundation for your web app's development.
                         </p>
                     </div>
-                </div> */}
+                </div>
             </div>
 
             <div style={{ backgroundImage: 'url(/assets/images/Rectangle80.png)' }} className="flex flex-col items-center w-full px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] pt-[121px] bg-no-repeat bg-cover">
                 <div className="flex flex-col lg:flex-row items-start justify-between">
                     <div className="w-full lg:w-1/2 flex flex-col">
-                        <p className="text-[38px] font-[700] max-w-[509px] mb-[30px]">
+                        <p className="text-[28px] sm:text-[32px] md:text-[35px] lg:text-[38px] font-[700] max-w-[509px] mb-[30px]">
                             Benefits Of Building Your Web App With Us
                         </p>
                         <div className="flex flex-row mb-[20px] gap-x-[16px]">
-                            <img src="/assets/icons/integrity_icon.svg" className="h-[40px] w-[40px]" />
-                            <p className="text-[24px] font-[400] font-[Montserrat] text-[#252525]">
+                            <img src="/assets/icons/integrity_icon.svg" className="h-[32px] lg:h-[40px] w-[32px] lg:w-[40px]" />
+                            <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[400] font-[Montserrat] text-[#252525]">
                                 Deployemnt
                             </p>
                         </div>
                         <div className="flex flex-row mb-[20px] gap-x-[16px]">
-                            <img src="/assets/icons/integrity_icon.svg" className="h-[40px] w-[40px]" />
-                            <p className="text-[24px] font-[400] font-[Montserrat] text-[#252525]">
+                            <img src="/assets/icons/integrity_icon.svg" className="h-[32px] lg:h-[40px] w-[32px] lg:w-[40px]" />
+                            <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[400] font-[Montserrat] text-[#252525]">
                                 Scalable and reliable sulutions
                             </p>
                         </div>
                         <div className="flex flex-row mb-[20px] gap-x-[16px]">
-                            <img src="/assets/icons/integrity_icon.svg" className="h-[40px] w-[40px]" />
-                            <p className="text-[24px] font-[400] font-[Montserrat] text-[#252525]">
+                            <img src="/assets/icons/integrity_icon.svg" className="h-[32px] lg:h-[40px] w-[32px] lg:w-[40px]" />
+                            <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[400] font-[Montserrat] text-[#252525]">
                                 Flexible and cost-effective development
                             </p>
                         </div>
                         <div className="flex flex-row mb-[20px] gap-x-[16px]">
-                            <img src="/assets/icons/integrity_icon.svg" className="h-[40px] w-[40px]" />
-                            <p className="text-[24px] font-[400] font-[Montserrat] text-[#252525]">
+                            <img src="/assets/icons/integrity_icon.svg" className="h-[32px] lg:h-[40px] w-[32px] lg:w-[40px]" />
+                            <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[400] font-[Montserrat] text-[#252525]">
                                 Money back guarantee
                             </p>
                         </div>
@@ -155,17 +155,17 @@ const WebDev: FC = () => {
                     </div>
                     <img src="/assets/images/benefits.svg" className="w-full lg:w-1/2 max-h-[358px] max-w-[552px]" />
                 </div>
-                <p className="text-[38px] font-[700] text-[#030303] font-[Montserrat] max-w-[755px] text-center mt-[140px]">
+                <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#030303] font-[Montserrat] max-w-[755px] text-center mt-[140px]">
                     Want The Most Cost-Effective Web App Development?
                 </p>
-                <p className="text-[18px] font-[400] text-black font-[Montserrat] max-w-[767px] text-center mt-[18px]">
+                <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] text-black font-[Montserrat] max-w-[767px] text-center mt-[18px]">
                     You're in the right place! Fill out the form and we will reach out to discuss how we can best help your business grow.
                 </p>
             </div>
 
 
             <div className="flex flex-col items-center justify-center w-full">
-                <div className="max-w-[582px] p-[34px] shadow-2xl shadow-[#e0e0e0] bg-white border-[1px] border-[#e3e3e3] rounded-[20px] mt-[60px]">
+                <div className="max-w-[582px] w-[90%] p-[34px] shadow-2xl shadow-[#e0e0e0] bg-white border-[1px] border-[#e3e3e3] rounded-[20px] mt-[60px]">
                     <div className=" grid grid-cols-1 md:grid-cols-2 gap-x-[16px]">
                         <div className="mb-[17px]">
                             <p className="text-[16px] font-[400] font-[Montserrat] text-black mb-[10px] ml-[4px]">
@@ -198,7 +198,7 @@ const WebDev: FC = () => {
                         </p>
                         <textarea placeholder="How we can help you?" className="w-full border-[1px] text-[16px] font-[400] text-[#bebebe] border-[#e3e3e3] rounded-[20px] py-[18px] px-[26px] outline-none h-[95px]" />
                     </div>
-                    <button className="rounded-[20px] bg-[#006fff] w-full h-[53px] text-[18px] outline-none font-[400] text-white">
+                    <button className="rounded-[20px] bg-[#006fff] w-full h-[53px] text-[16px] sm:text-[17px] lg:text-[18px] outline-none font-[400] text-white">
                         Submit
                     </button>
                     <p className="font-[300] text-[14px] text-black font-[Montserrat] mt-[12px]">
