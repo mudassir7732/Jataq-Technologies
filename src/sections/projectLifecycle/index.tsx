8695f1e27a11aa686350ec4944e52c4972f9f6cf
mudassir7732 <mudassir7732@gmail.com>
2024-06-29 17:45:24 +0500

    Updated design

--- src/sections/projectLifecycle/index.tsx
+++ src/sections/projectLifecycle/index.tsx
@@ -8,11 +8,9 @@ const ProjectLifecycle: FC<LifecycleProps> = (props) => {
     return (
         <div className="w-full mt-[107px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px]">
             <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] font-[Montserrat] max-w-[835px] mx-auto text-center">
-                {/* Managing The Full Software Development Lifecycle */}
                 {props.title}
             </p>
             <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] max-w-[667px] mx-auto font-[Montserrat] text-center mt-[17px]">
-                {/* Jataq Technologies provides end-to-end web app development. */}
                 {props.desc}
             </p>
 
