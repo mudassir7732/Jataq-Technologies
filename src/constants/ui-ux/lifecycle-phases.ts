95394d35970a6aee426eaff10804901aa201bf81
mudassir7732 <mudassir7732@gmail.com>
2024-06-29 18:01:19 +0500

    Updated design

--- src/constants/ui-ux/lifecycle-phases.ts
+++ src/constants/ui-ux/lifecycle-phases.ts
@@ -2,9 +2,9 @@ import { TopSectionProps } from "../../types/topSection";
 
 export const Phases: TopSectionProps[] = [
     { title: 'Dicovery & Research', desc: "We begin by understanding your business goals, target audience, and market landscape. This involves comprehensive research and stakeholder interviews to gather valuable insights." },
-    { title: 'Strategy & Planning', desc: "Based on our findings, we develop a customized digital marketing strategy that outlines goals, tactics, and key performance indicators (KPIs). This ensures a clear path to success." },
-    { title: 'Wireframing & Prototyping', desc: "Our team creates compelling marketing campaigns across various digital channels. We continuously monitor and optimize these campaigns to maximize results and ROI." },
-    { title: 'Visual Design', desc: "We develop engaging and relevant content that resonates with your audience and supports your marketing goals. This includes blogs, social media posts, videos, and more." },
-    { title: 'User Testing & Feedback', desc: "We track the performance of your campaigns using advanced analytics tools. Regular reports provide insights into what's working and where improvements can be made." },
-    { title: 'Implementation & Development Support', desc: "Our process is iterative. We use data-driven insights to continuously refine and enhance your digital marketing efforts, ensuring sustained growth and success." }
+    { title: 'Strategy & Planning', desc: "Based on our findings, we develop a strategic plan that outlines the project scope, objectives, and key milestones. This phase ensures that everyone is aligned and clear on the project's direction." },
+    { title: 'Wireframing & Prototyping', desc: "Our designers create wireframes and interactive prototypes to visualize the structure and flow of your application. This step is crucial for identifying potential issues early and refining the user journey." },
+    { title: 'Visual Design', desc: "We then craft stunning visual designs that reflect your brand identity. Our team focuses on creating aesthetically pleasing and functional designs that resonate with your audience." },
+    { title: 'User Testing & Feedback', desc: "To ensure our designs meet user expectations, we conduct usability testing with real users. Feedback is gathered and incorporated into the design to make necessary improvements." },
+    { title: 'Implementation & Development Support', desc: "Our collaboration doesn't end with design. We work closely with your development team to ensure that the designs are implemented accurately and efficiently. We provide ongoing support to address any challenges that arise during development." }
 ]
