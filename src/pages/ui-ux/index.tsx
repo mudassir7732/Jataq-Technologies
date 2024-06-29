efbb759224aa3a944fa673060c73efe205ac5454
mudassir7732 <mudassir7732@gmail.com>
2024-06-29 18:17:04 +0500

    Updated design

--- src/pages/ui-ux/index.tsx
+++ src/pages/ui-ux/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import Testimonials from "../../sections/testimonials";
 import ServicesBar from "../../sections/services-bar";
-import { FeatureProps } from "../../types/features";
 import FiguresBar from "../../components/figures-bar.tsx";
 import TopSection from "../../sections/top-section";
 import { data } from "../../constants/ui-ux/topSection";
