2d26b3668b84bc92667934d7558eb02de1e90e07
mudassir7732 <mudassir7732@gmail.com>
2024-06-29 19:17:08 +0500

    Set Borders for benefits

--- src/sections/projectLifecycle/index.tsx
+++ src/sections/projectLifecycle/index.tsx
@@ -11,12 +11,18 @@ const ProjectLifecycle: FC<LifecycleProps> = (props) => {
             <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] max-w-[667px] mx-auto font-[Montserrat] text-center mt-[17px]">
                 {props.desc}
             </p>
-
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-[38px] sm:mt-[45px] md:mt-[60px]">
                 {props.lifecyclePhases?.map((phase: LifecyclePhasesProps, index: number) => (
-                    <div className="border-[#d2d2d2] sm:pl-[24px] pt-0 sm pb-[15px] sm:pb-[29px] ">
+                    <div className={`border-[#d2d2d2] pb-[15px] sm:pb-[29px]
+                    ${index === 0 && 'sm:border-r-[1px] sm:border-b-[1px] pt-0 sm:pr-[13px]'}
+                    ${index === 1 && 'lg:border-r-[1px] sm:border-b-[1px] pt-0 sm:pl-[13px] lg:px-[20px] xl:px-[38px]'}
+                    ${index === 2 && 'sm:border-r-[1px] lg:border-r-0 pt-0 sm:border-b-[1px] sm:pr-[13px] lg:pr-0 lg:pl-[20px] xl:pl-[38px]'}
+                    ${index === 3 && 'border-r-0 lg:border-r-[1px] sm:border-b-[1px] lg:border-b-0 sm:pt-[32px] sm:pl-[13px] lg:pl-0 lg:pr-[20px] xl:pr-[38px]'}
+                    ${index === 4 && 'sm:border-r-[1px] sm:pt-[32px] sm:pr-[13px] lg:px-[20px] xl:px-[38px]'}
+                    ${index === 5 && 'sm:pt-[32px] sm:pl-[13px] lg:pl-[20px] xl:pl-[38px]'}
+                    `}>
                         <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700] text-[#d2d2d2]">
-                            0{index+1}
+                            0{index + 1}
                         </p>
                         <p className="text-[19px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#252525] mt-[7px]">
                             {phase.title}
