62f47278e898294d7204e1c4727a71452d9e79d4
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 01:15:38 +0500

    Seperated styles file from the content file

--- src/components/section-title/index.tsx
+++ src/components/section-title/index.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { DescProps } from "../../types/pageDesc";
+import styles from "./styles";
 
 const SectionTitle: FC<DescProps> = ({ title, desc }) => {
     return (
         <>
-            <p className="text-[27px] sm:text-[30px] md:text-[34px] lg:text-[38px] font-[700] font-[Montserrat] text-black mt-[4px] md:mt-[8px] lg:mt-[11px] text-center w-full">
+            <p className={styles.title}>
                 {title}
             </p>
             <p className="text-[18px] font-[400] max-w-[567px] mx-auto text-center font-[Montserrat] mb-[63px]">
