22bf5d57737943099d8a643dc13eec5bc6702d8c
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 00:37:01 +0500

    Seperated styles file from the content file

--- src/sections/projectLifecycle/index.tsx
+++ src/sections/projectLifecycle/index.tsx
@@ -14,12 +14,7 @@ const ProjectLifecycle: FC<LifecycleProps> = (props) => {
             </p>
             <div className={styles.phases_wrapper}>
                 {props.lifecyclePhases?.map((phase: LifecyclePhasesProps, index: number) => (
-                    <div className={`${styles.phase} ${index === 0 && 'sm:border-r-[1px] sm:border-b-[1px] pt-0 sm:pr-[20px] xl:pr-[38px]'}
-                    ${index === 1 && 'lg:border-r-[1px] sm:border-b-[1px] pt-0 sm:pl-[20px] lg:px-[20px] xl:px-[38px]'}
-                    ${index === 2 && 'sm:border-r-[1px] lg:border-r-0 pt-0 sm:border-b-[1px] sm:pr-[20px] lg:pr-0 lg:pl-[20px] xl:pl-[38px]'}
-                    ${index === 3 && 'border-r-0 lg:border-r-[1px] sm:border-b-[1px] lg:border-b-0 sm:pt-[32px] sm:pl-[20px] lg:pl-0 lg:pr-[20px] xl:pr-[38px]'}
-                    ${index === 4 && 'sm:border-r-[1px] sm:pt-[32px] sm:pr-[20px] lg:px-[20px] xl:px-[38px]'}
-                    ${index === 5 && 'sm:pt-[32px] sm:pl-[20px] lg:pl-[20px] xl:pl-[38px]'} `}>
+                    <div className={`${styles.phase} ${index === 0 && styles.first_phase} ${index === 1 && styles.second_phase} ${index === 2 && styles.third_phase} ${index === 3 && styles.fourth_phase} ${index === 4 && styles.fifth_phase} ${index === 5 && styles.sixth_phase} `}>
                         <p className={`${styles.index} ${index === 0 ? 'text-[#006fff]' : 'text-[#d2d2d2]'}`}>
                             0{index + 1}
                         </p>
