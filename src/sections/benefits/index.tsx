7c87d0b8339542c4850ec3d66177e7cb5458f314
mudassir7732 <mudassir7732@gmail.com>
2024-06-30 18:41:22 +0500

    Seperated styles file from the content file

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -7,22 +7,22 @@ import styles from "./styles";
 const Benefits: FC<BenefitsProps> = (props) => {
     return (
         <div style={{ backgroundImage: 'url(/assets/images/Rectangle69.png)' }} className={styles.container}>
-            <div className="flex flex-col lg:flex-row items-start justify-between">
-                <div className="w-full lg:w-1/2 pr-[20px] lg:pr-0">
-                    <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700]">
+            <div className={styles.inner_container}>
+                <div className={styles.first_section}>
+                    <p className={styles.title}>
                         {props.title}
                     </p>
-                    <p className="mt-[19px] text-[16px] sm:text-[17px] lg:text-[18px] font-[400] text-black max-w-[88%]">
+                    <p className={styles.desc}>
                         {props.desc}
                     </p>
-                    <button className="flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[40px] sm:h-[45px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-4 lg:px-3 xl:w-[257px] bg-[#1b7eff] rounded-[10px] sm:rounded-[12px] md:rounded-[15px] lg:rounded-[20px] mt-[34px] text-[13px] md:text-[16px] xl:text-[16px] sm:text-[17px] lg:text-[18px] text-white font-[500] font-[Montserrat]">
+                    <button className={styles.button}>
                         Schedule a Meeting
-                        <img src='/assets/icons/arrow.svg' alt='arrow_icon' className="h-[12px] sm:h-[13px] md:h-[14px] lg:h-[15px] xl:h-[16.25px]" />
+                        <img src='/assets/icons/arrow.svg' alt='arrow_icon' className={styles.button_icon} />
                     </button>
                 </div>
-                <div className="w-full lg:w-1/2 flex flex-col gap-y-[18px] mt-12 lg:mt-0">
+                <div className={styles.second_section}>
                     {props.features?.map((feature: FeatureProps, index: number) => (
-                        <div key={index} className={`flex flex-row items-center gap-x-[20px] md:gap-x-[39px] px-[15px] sm:px-[18px] md:px-[22px] lg:px-[27px] py-[12px] sm:py-[17px] md:py-[24px] lg:py-[31px] ${index === 0 ? 'bg-[#006fff]' : 'bg-white'} rounded-[20px]`}>
+                        <div key={index} className={`${styles.feature} ${index === 0 ? 'bg-[#006fff]' : 'bg-white'}`}>
                             <img src={feature?.icon} className={`${index === 0 ? 'w-[30px] md:w-[39px]' : 'w-[42px] '} ${index === 0 ? 'h-[30px] md:h-[39px]' : 'h-[42px] md:h-[55px]'}`} />
                             <p className={`text-[22px] font-[500] font-[Montserrat] ${index === 0 ? 'text-white' : 'text-black'}`}>
                                 {feature?.label}
--- src/sections/benefits/styles.ts
+++ src/sections/benefits/styles.ts
@@ -1,5 +1,12 @@
 const styles = {
     container:"bg-center flex flex-col  bg-no-repeat bg-cover mb-[50px] px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px] gap-y-[116px] pt-[36px] sm:pt-[42px] md:pt-[48px] lg:pt-[55px] xl:pt-[66px]",
-    
+    inner_container:"flex flex-col lg:flex-row items-start justify-between",
+    first_section:"w-full lg:w-1/2 pr-[20px] lg:pr-0",
+    second_section:"w-full lg:w-1/2 flex flex-col gap-y-[18px] mt-12 lg:mt-0",
+    title:"text-[25px] sm:text-[30px] md:text-[35px] lg:text-[38px] font-[700]",
+    desc:"mt-[19px] text-[16px] sm:text-[17px] lg:text-[18px] font-[400] text-black max-w-[88%]",
+    button:"flex flex-row items-center justify-center gap-x-[6px] lg:gap-x-[11px] h-[40px] sm:h-[45px] md:h-[48px] lg:h-[51px] xl:h-[53px] w-fit px-4 lg:px-3 xl:w-[257px] bg-[#1b7eff] rounded-[10px] sm:rounded-[12px] md:rounded-[15px] lg:rounded-[20px] mt-[34px] text-[13px] md:text-[16px] xl:text-[16px] sm:text-[17px] lg:text-[18px] text-white font-[500] font-[Montserrat]",
+    button_icon:"h-[12px] sm:h-[13px] md:h-[14px] lg:h-[15px] xl:h-[16.25px]",
+    feature:"flex flex-row items-center gap-x-[20px] md:gap-x-[39px] px-[15px] sm:px-[18px] md:px-[22px] lg:px-[27px] py-[12px] sm:py-[17px] md:py-[24px] lg:py-[31px] rounded-[20px]"
 }
 export default styles;
