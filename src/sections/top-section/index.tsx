1481f0bf9437c7aa0feb6f39b59777b26e94ada1
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 00:39:54 +0500

    Seperated styles file from the content file

--- src/sections/top-section/index.tsx
+++ src/sections/top-section/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { TopSectionProps } from "../../types/topSection";
+import styles from "./styles";
 
 const TopSection: FC<TopSectionProps> = ({ title, desc }) => {
     return (
-        <div className='w-full px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px]'>
-            <p className="text-[35px] sm:text-[40px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-[700] mx-auto w-full text-center font-[Montserrat] text-[#000000] mt-[76px]">
+        <div className={styles.container}>
+            <p className={styles.title}>
                 {title}
             </p>
-            <p className="text-[16px] sm:text-[17px] lg:text-[18px] font-[400] max-w-[775px] mx-auto text-center font-[Montserrat] mb-[63px] mt-4 sm:mt-2 md:mt-0">
+            <p className={styles.desc}>
                 {desc}
             </p>
         </div>
---
+++ src/sections/top-section/styles.ts
@@ -0,0 +1,6 @@
+const styles = {
+    container:"w-full px-[30px] sm:px-[60px] md:px-[82px] lg:px-[95px] xl:px-[115px]",
+    title:"text-[35px] sm:text-[40px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-[700] mx-auto w-full text-center font-[Montserrat] text-[#000000] mt-[76px]",
+    desc:"text-[16px] sm:text-[17px] lg:text-[18px] font-[400] max-w-[775px] mx-auto text-center font-[Montserrat] mb-[63px] mt-4 sm:mt-2 md:mt-0"
+}
+export default styles;
