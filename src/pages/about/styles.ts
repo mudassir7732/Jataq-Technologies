cba1bab2307b21ab22a7ca40f7ab4ec95c4125c3
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 00:58:16 +0500

    Seperated styles file from the content file

--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -4,7 +4,7 @@ const styles = {
     text_wrapper:"w-full flex items-center justify-center px-[25px] xl:px-[49px]",
     text:"text-[25px] md:text-[28px] lg:text-[30px] max-w-[519px] font-[700] py-[30px] text-white font-[Montserrat]",
     image:"w-full sm:max-w-[570px] md:min-w-[584px]",
-    purposes:"flex flex-col md:flex-row items-start justify-between gap-x-[87px] mb-[50px] sm:mb-[60px] md:mb-[70px] lg:mb-[84px] gap-y-[40px] mt-[47px]",
+    purposes:"flex flex-col md:flex-row items-start justify-between gap-x-[87px] mb-[50px] mb-[50px] gap-y-[40px] mt-[47px]",
     purpose_wrapper:"flex flex-col w-full md:w-1/2",
 }
 export default styles;
