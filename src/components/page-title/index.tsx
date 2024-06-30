2c712b4c2fec6fa60f23ad3cac8f103684410983
mudassir7732 <mudassir7732@gmail.com>
2024-06-30 17:21:42 +0500

    Seperated styles file from the content file

---
+++ src/components/page-title/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const PageTitle: FC = () => {
+    return (
+        <>
+            <p className="text-[27px] sm:text-[30px] md:text-[34px] lg:text-[38px] font-[700] font-[Montserrat] text-black mt-[4px] md:mt-[8px] lg:mt-[11px] text-center w-full">
+                Our Journey Through Time
+            </p>
+            <p className="text-[18px] font-[400] max-w-[567px] mx-auto text-center font-[Montserrat] mb-[63px]">
+                Discover exceptional experiences through testimonials from our satisfied customers.
+            </p>
+        </>
+    )
+}
+export default PageTitle;
