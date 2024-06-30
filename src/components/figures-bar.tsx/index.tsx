93cd3e78f3b93cc75a1c15c2f64cc7329957917c
mudassir7732 <mudassir7732@gmail.com>
2024-06-30 17:15:06 +0500

    Seperated styles file from the content file

--- src/components/figures-bar.tsx/index.tsx
+++ src/components/figures-bar.tsx/index.tsx
@@ -1,7 +1,8 @@
+import styles from "./styles";
 
 const FiguresBar = () => {
     return (
-        <div style={{ backgroundImage: 'url(/assets/images/figures_bg.png)' }} className="px-[40px] sm:px-[70px] md:px-[80px] lg:px-[95px] xl:px-[113px] bg-no-repeat bg-cover bg-center items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-fit gap-x-[106px] gap-y-[40px] py-[46px] min-h-[156px] rounded-[10px]">
+        <div style={{ backgroundImage: 'url(/assets/images/figures_bg.png)' }} className={styles.container}>
             <div className="flex flex-col min-w-[185px]">
                 <p className="text-[26px] sm:text-[31px] md:text-[34px] lg:text-[36px] xl:text-[38px] font-[700] font-[Montserrat] text-black">
                     10+
---
+++ src/components/figures-bar.tsx/styles.ts
@@ -0,0 +1,6 @@
+const styles = {
+    container:"px-[40px] sm:px-[70px] md:px-[80px] lg:px-[95px] xl:px-[113px] bg-no-repeat bg-cover bg-center items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-fit gap-x-[106px] gap-y-[40px] py-[46px] min-h-[156px] rounded-[10px]",
+    item:"flex flex-col min-w-[185px]",
+    
+}
+export default styles;
