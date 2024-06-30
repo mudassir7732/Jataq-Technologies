c05fac15d8b36d22b5d7158b30003911cdefaf76
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 00:45:38 +0500

    Seperated styles file from the content file

--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -1,5 +1,6 @@
 const styles = {
     container:"w-full px-[40px] sm:px-[50px] md:px-[65px] lg:px-[80px] xl:px-[100px] pt-[76px] pb-[63px]",
+    card:"flex flex-col h-fit w-fit mx-auto xl:w-full xl:flex-row items-center justify-between gap-x-[22px] bg-[#006fff] rounded-[20px]",
     
 }
 export default styles;
