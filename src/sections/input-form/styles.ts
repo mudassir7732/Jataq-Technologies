b6f448c234679d7b0d2407bc8e638a3387d3b30b
mudassir7732 <mudassir7732@gmail.com>
2024-06-30 19:06:41 +0500

    Seperated styles file from the content file

--- src/sections/input-form/styles.ts
+++ src/sections/input-form/styles.ts
@@ -1,6 +1,10 @@
 const styles = {
     container:"flex flex-col items-center justify-center w-full",
     inner_container:"max-w-[582px] w-[90%] p-[34px] shadow-2xl shadow-[#e0e0e0] bg-white border-[1px] border-[#e3e3e3] rounded-[20px] mt-[60px]",
-    
+    inputs_wrapper:"grid grid-cols-1 md:grid-cols-2 gap-x-[16px]",
+    label:"text-[16px] font-[400] font-[Montserrat] text-black mb-[10px] ml-[4px]",
+    input:"w-full border-[1px] outline-none text-[16px] font-[400] text-[#bebebe] border-[#e3e3e3] rounded-[20px] px-[26px] h-[53px]",
+    button:"rounded-[20px] bg-[#006fff] w-full h-[53px] text-[16px] sm:text-[17px] lg:text-[18px] outline-none font-[400] text-white",
+    text:"font-[300] text-[14px] text-black font-[Montserrat] mt-[12px]"
 }
 export default styles;
