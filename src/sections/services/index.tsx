1a5176e406b8ead664368cacb5f92d65dbe307f3
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 01:22:04 +0500

    Updated Code

--- src/sections/services/index.tsx
+++ src/sections/services/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import React, { FC, useState } from "react";
 import { SERVICE_OPTIONS } from "../../constants/serviceOptions";
 import styles from "./styles";
 import styles2 from '../about-us/styles';
