d4c8ff32aa82ed718ae601a7648981eb5f63b0dd
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 00:38:11 +0500

    Seperated styles file from the content file

--- src/sections/testimonials/styles.ts
+++ src/sections/testimonials/styles.ts
@@ -7,6 +7,7 @@ const styles = {
     heading: "text-[26px] sm:text-[28px] md:text-[30px] lg:text-[35px] xl:text-[38px] font-[700] text-white font-[Montserrat] mt-[57px] max-w-[425px]",
     second_section: "flex flex-col max-w-full sm:max-w-[90%] md:max-w-[514px] mt-16 lg:mt-0",
     carousel_wrapper: "my-[6px] h-auto min-h-[300px] pt-8",
+    star:"h-[19px] lg:h-[22px] w-[19px] lg:w-[22px]",
     quotation: "absolute z-50 -mt-[58px] ml-3",
     card: "bg-white z-40 max-h-[249px] rounded-t-[10px] w-full md:max-w-[614px] pt-[25px] sm:pt-[30px] md:pt-[35px] lg:pt-[42px] xl:pt-[47px] px-[15px] pb-8 sm:px-[22px] md:px-[28px] lg:px-[35px] xl:px-[40px]",
     text: "text-[16px] font-[400] font-[Montserrat]",
