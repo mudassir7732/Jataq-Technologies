fd58b05b7abfd981ce9142572508b7bad4934e1f
mudassir7732 <mudassir7732@gmail.com>
2024-07-01 01:15:19 +0500

    Seperated styles file from the content file

--- src/components/section-title/styles.ts
+++ src/components/section-title/styles.ts
@@ -1,4 +1,4 @@
 const styles = {
-
+    title:"text-[27px] sm:text-[30px] md:text-[34px] lg:text-[38px] font-[700] font-[Montserrat] text-black mt-[4px] md:mt-[8px] lg:mt-[11px] text-center w-full",
 }
 export default styles;
